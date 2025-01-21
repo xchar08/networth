9de426bfe6529ce25b2147d0e122871a720a48fb
xchar08 <108827236+xchar08@users.noreply.github.com>
2025-01-21 02:55:07 -0600

    LETS GO IT DEPLOYED

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
-// frontend/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure this is set to 'dist'
-    // base: '/', // Optional: Ensure the base path is correct
-  },
+    outDir: 'dist',
+    emptyOutDir: true 
+  }
 });
